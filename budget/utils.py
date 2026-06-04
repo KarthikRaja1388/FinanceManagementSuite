f96d65290bf627b9495db5a9a2d78bbf416fd4ff
Karthik Rajendran <karthik.vbdon88@gmail.com>
2026-06-04 23:51:21 +1200

    insights logic and UI

--- budget/utils.py
+++ budget/utils.py
@@ -1,8 +1,9 @@
-from django.utils import timezone
+from itertools import count
 
 from django.db.models import Sum
 
 from budget.models import Budget
+from shopping.utils import get_most_used_category
 from transaction.models import Transaction
 
 
@@ -37,13 +38,30 @@ def get_alerts(user):
     for budget in active_budgets:
         expense = get_spent_amount(user, budget.start_date, budget.end_date, budget.category)
 
-        if budget.budget_amount <= expense:
+        budget_percent_used = get_budget_percentage_used(budget.budget_amount, expense)
+
+        if budget_percent_used >= 100:
             exceeded_by = expense - budget.budget_amount
             alerts.append({
                 "message": (
-                    f"Budget <b>{budget.budget_name}</b> exceeded by <b>{exceeded_by}</b> "
-                    f"(limit: {budget.budget_amount})"
-                ),
+                    f" You have exceeded your <b>{budget.budget_name} budget</b> by <b>{exceeded_by}</b>. Review your recent spending to stay on track." ),
                 "alert_type":"negative"
             })
+        elif budget_percent_used >= 80:
+            alerts.append({
+                "message": (f"You have used 80% of your {budget.budget_name} budget. Keep an eye for the rest of the period."),
+                "alert_type": "warning"
+            })
     return alerts
+
+def get_insights(user, start_date, end_date):
+    insights = []
+
+    most_used_category = get_most_used_category(user, start_date, end_date)
+
+    if most_used_category:
+        insights.append({
+            "message": f"{most_used_category['category__category_name']} was your most frequent purchase category with {most_used_category['used']} transactions."
+        })
+
+    return insights
